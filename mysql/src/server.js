bcadc099d65ff4fc3e8f8aad11b99da60bab8c82
Abhishek Silawat <absilawat@gmail.com>
2022-10-17 11:05:14 +0530

    Update server.js

--- mysql/src/server.js
+++ mysql/src/server.js
@@ -1,18 +1,15 @@
-const app = require("./app.js")
-const mysqlConnection = require("./configs/mysql")
+const app = require("./app.js");
+const mysqlConnection = require("./configs/mysql");
 
 mysqlConnection.connect((err) => {
-    if(!err){
-        console.log("Connected!!!")
-    }
-    else {
-        console.log(err)
-        console.log("Connection Failed")
-    }
-})
-
-
+  if (!err) {
+    console.log("Connected!!!");
+  } else {
+    console.log(err);
+    console.log("Connection Failed");
+  }
+});
 
 app.listen(3000, () => {
-    console.log("Express server is running at PORT 3000")
-})
+  console.log("Express server is running at PORT 3000");
+});
