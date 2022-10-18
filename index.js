97fe11780dd98c02a70f0898e03577a5903b04ef
Abhishek Silawat <absilawat@gmail.com>
2022-10-18 21:58:15 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// let str = "masai";
+// let arr = [...str];
+// console.log(arr);
+// var a;
+// function x() {
+//   a = 10;
+// }
+// console.log(a);
+
+const arr = new Array(4).fill([]);
+arr[2].push(5);
+console.log(arr);
+let obj = () => {
+  this.a = "pandey";
+};
+obj();
+console.log(a);
