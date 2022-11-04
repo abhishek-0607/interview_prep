d4f87140ef641e9b54f370bd1323a6b20f184c4e
Abhishek Silawat <absilawat@gmail.com>
2022-11-04 23:01:25 +0530

    Update rotateArray.js

--- dsa-questions/rotateArray.js
+++ dsa-questions/rotateArray.js
@@ -1,17 +1,17 @@
 // Given an array of integers arr[] of size N and an integer,
-//  the task is to rotate the array elements to the right by d positions.
+//  the tasd is to rotate the array elements to the right by d positions.
 
 var arr = [1, 2, 3, 4, 5, 6, 7];
-var k = 2;
-var temp = [];
-for (var i = 0; i < arr.length; i++) {
-  if (i < k) {
-    temp.push(arr[arr.length - k + i]);
-  } else {
-    temp.push(arr[i - k]);
-  }
-}
-console.log(temp);
+var d = 2;
+// var temp = [];
+// for (var i = 0; i < arr.length; i++) {
+//   if (i < d) {
+//     temp.push(arr[arr.length - d + i]);
+//   } else {
+//     temp.push(arr[i - d]);
+//   }
+// }
+// console.log(temp);
 
 // juggling algorithm
 
@@ -21,3 +21,19 @@ function gcd(a, b) {
   }
   return gcd(b, a % b);
 }
+d = d % arr.length;
+
+for (let i = 0; i < gcd(d, arr.length); i++) {
+  let temp = arr[i];
+  let j = i;
+  while (1) {
+    let k = j + d;
+    if (k >= arr.length) k = k - arr.length;
+    if (k == i) break;
+
+    arr[j] = arr[k];
+    j = k;
+  }
+  arr[j] = temp;
+}
+console.log(arr);
