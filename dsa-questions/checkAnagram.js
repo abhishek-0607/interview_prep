0eb6e6331030e4441724b904fe8b6d3dfb4b0380
Abhishek Silawat <absilawat@gmail.com>
2022-11-11 13:31:52 +0530

    Update checkAnagram.js

--- dsa-questions/checkAnagram.js
+++ dsa-questions/checkAnagram.js
@@ -31,3 +31,34 @@ function checkAnagram(str1, str2) {
 }
 
 console.log(checkAnagram(str1, str2));
+
+function isAnagram(str1, str2) {
+  // code here
+  var obj = {};
+
+  if (str1.length != str2.length) {
+    return false;
+  }
+  for (var i = 0; i < str1.length; i++) {
+    if (obj[str1[i]] == undefined) {
+      obj[str1[i]] = 1;
+    } else {
+      obj[str1[i]]++;
+    }
+  }
+  for (var i = 0; i < str2.length; i++) {
+    if (obj[str2[i]] == undefined) {
+      return false;
+    } else {
+      obj[str2[i]]--;
+    }
+  }
+
+  for (var k in obj) {
+    if (obj[k] != 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
