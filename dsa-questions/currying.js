046b70131ff25e6cebd32b29c798766157354587
Abhishek Silawat <absilawat@gmail.com>
2022-12-07 20:36:01 +0530

    Update currying.js

--- dsa-questions/currying.js
+++ dsa-questions/currying.js
@@ -8,3 +8,12 @@ function sum(a) {
 }
 var x = sum(1)(2)(3)(5)();
 console.log(x);
+
+function finiteSum(a) {
+  return function (b) {
+    return function (c) {
+      return a + b + c;
+    };
+  };
+}
+console.log(finiteSum(1)(3)(2));
