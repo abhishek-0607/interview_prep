23c1738f35e05be7e8d1ad4ad5a8ba3146c032c9
Abhishek Silawat <absilawat@gmail.com>
2022-12-13 13:11:29 +0530

    Update promises.js

--- promises.js
+++ promises.js
@@ -13,12 +13,13 @@ const cart = ["shoes", "shirt", "pant", "kurta"];
 //Promises
 //whenever js enigine exicute this line it's
 //create a empty data { data: undefined } then when it will fulfilled it filled the object { data }
-const URL = "https://api.github.com/users/akshaymarch7";
-const user = fetch(URL);
-console.log(user);
-user.then((res) => {
-  console.log(res);
-});
+
+// const URL = "https://api.github.com/users/akshaymarch7";
+// const user = fetch(URL);
+// console.log(user);
+// user.then((res) => {
+//   console.log(res);
+// });
 
 // creating Promise object
 var myPromise = new Promise(function (resolve, reject) {
@@ -51,4 +52,50 @@ function processUserInput(callback) {
   callback(name);
 }
 
-processUserInput(greeting);
+// processUserInput(greeting);
+
+//example of promise
+const sleep = (time) => {
+  return new Promise((res, rej) => {
+    if (typeof time !== "number") {
+      rej("argument is not a number");
+    }
+    setTimeout(() => {
+      res(`sleep in ${time / 1000}sec`);
+    }, time);
+  });
+};
+// Parallel requests
+function parallel() {
+  return Promise.all([
+    sleep(100),
+    sleep(500)
+      .then((res) => {
+        throw new Error("error message");
+      })
+      .catch((err) => err),
+    sleep(1000),
+    sleep(3000),
+  ])
+    .then((res) => res)
+    .catch((err) => err);
+}
+// parallel().then((res) => console.log(res));
+
+async function test1() {
+  console.log("SEQUENCIAL START");
+  let res1 = await sleep(3000);
+  console.log(res1, 1);
+  let res2 = await sleep(1000);
+  console.log(res2, 2);
+}
+test1();
+async function test2() {
+  console.log("CONCURRENT START");
+  let res1 = await sleep(3000);
+  let res2 = await sleep(1000);
+  console.log(res1, 3);
+  console.log(res2, 4);
+}
+
+test2();
