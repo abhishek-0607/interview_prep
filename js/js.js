d16747b3552c8260c714b1fc3bb474fe12205866
Abhishek Silawat <absilawat@gmail.com>
2023-03-22 23:28:52 +0530

    Create js.js

---
+++ js/js.js
@@ -0,0 +1,15 @@
+// for (let i = 1; i <= 5; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, 1000);
+// }
+console.log("" || null || 0);
+
+function sum(a) {
+  return function (b) {
+    return function (c) {
+      return a + b + c;
+    };
+  };
+}
+console.log(sum(1)(2)(3));
